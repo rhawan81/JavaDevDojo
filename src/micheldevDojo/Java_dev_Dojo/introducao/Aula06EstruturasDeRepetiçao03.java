ccbb3a178c6bcdfd2c274151a49b11dae244ba5a
Michel <rhawanjs@gmail.com>
2026-02-26 20:44:14 -0300

    Estruturas de repeticao part3

---
+++ src/micheldevDojo/Java_dev_Dojo/introducao/Aula06EstruturasDeRepetiçao03.java
@@ -0,0 +1,17 @@
+package micheldevDojo.Java_dev_Dojo.introducao;
+
+public class Aula06EstruturasDeRepetiçao03 {
+    // Imprima os primeiros 25 numeros de um dado valor, por exemplo 50
+    public static void main(String[] args) {
+        int valorMax = 50;
+        for (int i = 0; i < valorMax ; i++) {
+            if (i> 25){
+                break;
+
+            }
+            System.out.println(i );
+
+        }
+
+    }
+}
