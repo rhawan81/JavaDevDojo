eed41a861c366915aeee8a1aba50ca4ab87678c9
Michel <rhawanjs@gmail.com>
2026-02-26 20:44:06 -0300

    Estruturas de repeticao part2

---
+++ src/micheldevDojo/Java_dev_Dojo/introducao/Aula06EstruturasDeRepetiçao02MaisExercicio.java
@@ -0,0 +1,14 @@
+package micheldevDojo.Java_dev_Dojo.introducao;
+
+public class Aula06EstruturasDeRepetiçao02MaisExercicio {
+    public static void main(String[] args) {
+        // Imprima todos os numeros pares de 0 até 1000000
+
+        for(int conte = 0;conte <= 10000000;conte++){
+            if(conte % 2 == 0){
+                System.out.println(conte);
+            }
+
+        }
+    }
+}
