1151d5ba201088797e517c8ace9378842343d3d2
michel <rhawanjs@gmail.com>
2026-03-06 20:45:34 -0300

    adiçao de metodo com retorno e sem retorno usando void

--- src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/dominio/Calculadora.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/dominio/Calculadora.java
@@ -7,10 +7,35 @@ public class Calculadora {
 
 
     public void subtracaoDoisNumeros(){
+
         System.out.println(21 - 2);
     }
 
     public void multiplicaDoisNumeros(int num1, int num2){
         System.out.println(num1 * num2);
     }
+    public double  divideDoisNumeros(double num1 , double num2){
+        if(num2 == 0){
+            return 0;
+        }
+        return num1 / num2;
+
+    }
+
+    public double  divideDoisNumeros02(double num1 , double num2){
+        if(num2 != 0){
+            return 0;
+        }
+        System.out.println(" Nao existe divisao por zero ");
+        return 0;
+    }
+
+    public void imprimaDivisao(double num1, double num2){
+        if(num2 == 0){
+            System.out.println(" Nao existe divisao por zero !");
+        }
+        System.out.println(num1 /num2);
+    }
 }
+
+
---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/test/CalculadoraTest03.java
@@ -0,0 +1,16 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.test;
+
+import micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double result = calculadora.divideDoisNumeros( 20,0);
+        System.out.println(result);
+        System.out.println(" --------------------------------");
+        calculadora.imprimaDivisao(86,5);
+
+
+    }
+}
