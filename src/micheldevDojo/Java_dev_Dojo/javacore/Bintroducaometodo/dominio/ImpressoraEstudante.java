096d754ac4396d56abc01b21a43ec8666b96aeff
Michel <rhawanjs@gmail.com>
2026-03-09 22:37:17 -0300

    new files

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/dominio/ImpressoraEstudante.java
@@ -0,0 +1,11 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio;
+
+public class ImpressoraEstudante {
+    // Método público que recebe um objeto Estudante como parâmetro (por referência)
+    public void ImprimeEstudante( Estudante estudante){
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+
+}
--- src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/test/EstudanteTest01.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/test/EstudanteTest01.java
@@ -1,11 +1,13 @@
 package micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.test;
 
 import micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio.Estudante;
+import micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante1 = new Estudante();
         Estudante estudante2 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
         estudante1.nome = "Jose";
         estudante1.idade = 19;
         estudante1.sexo = 'M';
@@ -14,17 +16,9 @@ public class EstudanteTest01 {
         estudante2.idade = 20;
         estudante2.sexo = 'F';
 
-
-        System.out.println(estudante1.nome);
-        System.out.println(estudante1.idade);
-        System.out.println(estudante1.sexo);
-
-        System.out.println("---------------------");
-
-
-        System.out.println(estudante2.nome);
-        System.out.println(estudante2.idade);
-        System.out.println(estudante2.sexo);
+// Criamos uma classe onde imprime o objeto de referencia que é estudante e eliminamos redundancia deixando o codigo mais coeso.
+        impressora.ImprimeEstudante(estudante1);
+        impressora.ImprimeEstudante(estudante2);
 
     }
 }
