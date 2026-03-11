651cb1a3302bead63c5ecc9c2e4a0ce3b104670b
Michel <rhawanjs@gmail.com>
2026-03-10 23:20:56 -0300

    alterações

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/ContaBancaria.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/ContaBancaria.java
@@ -1,12 +1,16 @@
 package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
 
-public class Banco {
-    public String nome;
-    public int saldo;
+public class ContaBancaria {
+    public String titular;
+    public double saldo;
 
 
 
     public void depositar(int valor){
+        if(valor <= 0){
+            System.out.println("Valor de deposito invalido!");
+            return;
+        }
         saldo += valor;
         System.out.println("Deposito de : "+valor+ " Foi efetuado com sucesso !");
 
@@ -15,6 +19,7 @@ public class Banco {
         if(valorSaque > saldo){
             System.out.println(" Valor insuficiente Para saque , faça um novo deposito");
         }
+
         else {
             saldo -= valorSaque;
             System.out.println("Voce sacou o valor de: " + valorSaque +"$"+" Com sucesso !");
