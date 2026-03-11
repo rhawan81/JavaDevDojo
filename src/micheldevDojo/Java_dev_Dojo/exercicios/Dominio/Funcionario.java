4a2e3faa75a7821e544cbdea6bfa45a34208a70d
Michel <rhawanjs@gmail.com>
2026-03-10 22:29:59 -0300

    pequenas correçoes

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Funcionario.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Funcionario.java
@@ -3,13 +3,13 @@ package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
 public class Funcionario {
 
     public String nome;
-    public int age;
+    public int idade;
     public double salario;
 
 
-    public void ImprimirDados(){
+    public void imprimirDados(){
         System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + age);
+        System.out.println("Idade: " + idade);
         System.out.println("Salario: " + salario);
     }
 }
