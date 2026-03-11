d06e4990e36ac6fe5ee91e7d3c1890f2adfe8f6a
Michel <rhawanjs@gmail.com>
2026-03-10 23:20:41 -0300

    alterações

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Alunos02.java
@@ -0,0 +1,15 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
+
+public class Alunos02 {
+    public String nome;
+    public int idade;
+    public double notas;
+
+    public void imprimirDados(){
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("notas: "+notas);
+    }
+
+
+}
