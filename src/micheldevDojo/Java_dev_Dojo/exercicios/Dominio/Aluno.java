eeaac62f5a34d9abc28e709823188e67ac0829c8
Michel <rhawanjs@gmail.com>
2026-03-10 22:30:15 -0300

    pequenas correçoes

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Aluno.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Aluno.java
@@ -2,19 +2,21 @@ package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
 
 public class Aluno {
     public String nome;
-    public int nota1 ;
-    public int nota2 ;
-    public int nota3 ;
+    public double nota1 ;
+    public double nota2 ;
+    public double nota3 ;
 
 
 
     public void calcularMedia(){
-        int mediaAluno = (nota1 + nota2 + nota3) / 3;
+        double mediaAluno = (nota1 + nota2 + nota3) / 3;
         if(mediaAluno >= 7){
             System.out.println("Aluno Aprovado");
         }
         else{
             System.out.println("Aluno Reprovado !!! ");
         }
+
+        System.out.println(" Media do Aluno foi de: " + mediaAluno);
     }
 }
