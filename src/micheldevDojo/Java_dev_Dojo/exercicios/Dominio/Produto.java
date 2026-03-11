b4ba93a8d83a9f341e882e45acce054a4f2971e2
Michel <rhawanjs@gmail.com>
2026-03-10 23:21:17 -0300

    alterações

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Produto.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Produto.java
@@ -6,17 +6,22 @@ public class Produto {
     public int qntd;
 
 
-    public void adicionarEstoque(int quantidade , String nomeProd){
-        qntd = quantidade;
-        System.out.println(qntd + " Produto adicionado: " + nomeProd);
+    public void adicionarEstoque(int quantidade){
+        qntd += quantidade;
+        System.out.println(quantidade + " produto(s) adicionado ao estoque: ");
     }
     public void removerEstoque(int quantidade){
+        if(quantidade > qntd){
+            System.out.println(" Estoque insuficiente !");
+            return;
+        }
         qntd -= quantidade;
         System.out.println("Produto Removido com sucesso !");
 
     }
     public void exibirEstoque(){
-        System.out.println("Produtos Disponiveis : " + nome + " qntd"+qntd);
+        System.out.println("Produtos  : " + nome );
+        System.out.println("Quantidade em estoque: " + qntd);
 
     }
 
