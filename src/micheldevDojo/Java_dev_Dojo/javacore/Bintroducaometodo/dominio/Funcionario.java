01b9dbf97199c9d8120afb52b09c67637588c89c
Michel <rhawanjs@gmail.com>
2026-03-12 20:55:36 -0300

    refatora a classe Funcionario para encapsular atributos e adiciona métodos getters e setters; atualiza testes correspondentes

--- src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/dominio/Funcionario.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media = 0;
 
 
     public void imprimaDados() {
@@ -27,12 +28,39 @@ public class Funcionario {
             return;
         }
         for (double salario : salarios) {
-            media += salario;
+            this.media += salario;
         }
 
-        media /= salarios.length;
-        System.out.println("\nMedia salarial " + media);
+        this.media /= salarios.length;
+        System.out.println("\nMedia salarial " + this.media);
+    }
+
+    public double getMedia() {
+        return media;
     }
 
 
+    public double[] getSalarios() {
+        return this.salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/test/FuncionarioTest01.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Bintroducaometodo/test/FuncionarioTest01.java
@@ -5,10 +5,12 @@ import micheldevDojo.Java_dev_Dojo.javacore.Bintroducaometodo.dominio.Funcionari
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Michel";
-        funcionario.idade = 29;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
-       funcionario.imprimaDados();
+        funcionario.setNome("Michel");
+        funcionario.setIdade(20);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
+        funcionario.imprimaDados();
         funcionario.imprimeMediaSalarial();
+        System.out.println(" Media salarial foi de : "+ funcionario.getMedia());
+
     }
 }
