6c3915919922a9a53f41b13272344d6bf623ea4d
Michel <rhawanjs@gmail.com>
2026-03-17 22:01:31 -0300

    refatora a classe Calculadora para utilizar varargs em métodos de cálculo e adiciona testes correspondentes

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Calculadora.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Calculadora.java
@@ -5,8 +5,68 @@ public class Calculadora {
     public int numero2;
     public int numero3;
 
+// REFATORAÇAÕ USANDO VAR ARGS e fazendo exercicios
+// ache o menor numero usando varargs
+    public int menorNum(int ... numero){
+        int menorNum = numero[0];
+        for(int num: numero){
+            if(num < menorNum){
+                menorNum = num;
+            }
+        }
+        return menorNum;
+    }
 
+    public double media(double ... numero){
+        double media = 0;
+        for (int i = 0; i < numero.length ; i++) {
+            media = numero[i] / numero.length;
 
+        }
+        return media;
+    }
+    public void maiorEMenor(int... numeros) {
+
+        if (numeros.length == 0) {
+            System.out.println("Nenhum número foi informado");
+            return;
+        }
+
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        for (int num : numeros) {
+            if (num > maior) {
+                maior = num;
+            }
+            if (num < menor) {
+                menor = num;
+            }
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+    }
+
+    public int contarPares(int ... numeros){
+        int contarNums = 0;
+        for(int contar: numeros){
+            if( contar % 2== 0){
+                contarNums++;
+            }
+        }
+        return contarNums;
+    }
+
+    public int maior(int ... numero){
+        int maior = numero[0];
+        for (int i = 0; i < numero.length ; i++) {
+            maior = numero[i];
+        }
+        return maior;
+    }
+
+    // metodo valido porem se o input de numeros aumentar eu irei ter que adicionar um por um novamente. ja com o varargs não.
     public int soma(int numero, int numero2){
         return numero + numero2;
     }
---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/CalculadoraTest02.java
@@ -0,0 +1,19 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Test;
+
+import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        int numeroMaior = calculadora.maior(10,20,30,40,50,800);
+        int numeroMenor = calculadora.menorNum(2,3,20,30);
+        double media = calculadora.media(29.0, 28.0 , 30.2);
+        int contarNums = calculadora.contarPares(1,2,3,4,5,6,7,8,9,10);
+        calculadora.maiorEMenor(1,2,3,4,5,6,7,8,9,10);
+        System.out.println("O numero menor numero é: " + numeroMenor);
+        System.out.println("O numero maior numero é: " + numeroMaior);
+        System.out.println("A media é: " + media);
+        System.out.println("A quantidade de numeros pares é: " + contarNums);
+    }
+}
