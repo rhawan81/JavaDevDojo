26927b6ec1a14e556eebb36967c3da6834bf82fd
Michel <rhawanjs@gmail.com>
2026-05-08 22:48:15 -0300

    fazendo exercicios

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Tabuada.java
@@ -0,0 +1,13 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
+
+public class Tabuada {
+
+
+    public void ImprimeTabuada(int numero){
+        for (int i = 0; i < 11 ; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+
+        }
+
+    }
+}
---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/TabuadaTest01.java
@@ -0,0 +1,11 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Test;
+
+import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.Tabuada;
+
+public class TabuadaTest01 {
+    public static void main(String[] args) {
+        Tabuada tabuada = new Tabuada();
+
+        tabuada.ImprimeTabuada(10);
+    }
+}
