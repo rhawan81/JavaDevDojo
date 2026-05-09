05b87eb462487d627ce4921997d971781480dab7
Michel <rhawanjs@gmail.com>
2026-05-08 22:53:40 -0300

    fazendo exercicios

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/SomarDeUmAteCem.java
@@ -0,0 +1,15 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
+
+public class SomarDeUmAteCem {
+    int n ;
+
+
+    public int imprime(){
+        for (int i = 0; i <= 100; i++) {
+            n += i;
+            System.out.println(n);
+
+        }
+        return n;
+    }
+}
---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/SomarTest01.java
@@ -0,0 +1,10 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Test;
+
+import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.SomarDeUmAteCem;
+
+public class SomarTest01 {
+    public static void main(String[] args) {
+        SomarDeUmAteCem soma = new SomarDeUmAteCem();
+        soma.imprime();
+    }
+}
