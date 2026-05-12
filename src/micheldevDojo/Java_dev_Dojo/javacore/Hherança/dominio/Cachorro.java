5ca23ca39f8cd1a3a743a640262e8cb9fce7a72f
Michel <rhawanjs@gmail.com>
2026-05-11 21:47:08 -0300

    Adiciona a classe Cachorro que estende Animal e inclui atributo raca

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Cachorro.java
@@ -0,0 +1,13 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Hherança.dominio;
+
+public class Cachorro extends Animal {
+    private String raca;
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}
