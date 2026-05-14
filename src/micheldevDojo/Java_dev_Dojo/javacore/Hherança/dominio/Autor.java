b61f9e4e82a10813d322a4a6f3a818b77c7e1d8f
Michel <rhawanjs@gmail.com>
2026-05-14 20:44:18 -0300

    Adiciona a classe Autor que estende Pessoa e inclui atributo nacionalidade

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Autor.java
@@ -0,0 +1,17 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Hherança.dominio;
+
+public class Autor extends Pessoa{
+    private String nacionalidade;
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void imprime(){
+        super.imprime();
+        System.out.println("Minha nacionalidade é + " + this.nacionalidade);
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
