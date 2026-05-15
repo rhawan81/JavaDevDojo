87763e488f32c4d189bf3cf297ae2f06d8f962ed
Michel <rhawanjs@gmail.com>
2026-05-14 22:17:09 -0300

    refatorando as classes

--- src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Biblioteca.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Biblioteca.java
@@ -1,27 +1,26 @@
 package micheldevDojo.Java_dev_Dojo.javacore.Hherança.dominio;
 
 public class Biblioteca {
-    private String[] nomes;
+    private Livro[] livros;
 
-    public Biblioteca(String[] nome) {
-        this.nomes = nomes;
+    public Biblioteca(Livro[] livros) {
+        this.livros = livros;
     }
 
-
-
-    public void listarLivros(){
-        System.out.println("Livros disponiveis na biblioteca: ");
-        for (String nome : nomes) {
-            System.out.println(nome);
+    public void listarLivros() {
+        System.out.println("Livros disponíveis na biblioteca:");
+        for (Livro livro : livros) {
+            livro.imprimir();
+            System.out.println("---");
         }
     }
 
-
-    public void buscarPorAutor(String nome){
+    public void buscarPorAutor(String nome) {
         System.out.println("Buscando livros do autor: " + nome);
-        for (String nomeLivro : nomes) {
-            if(nomeLivro.contains(nome)){
-                System.out.println(nomeLivro);
+        for (Livro livro : livros) {
+            if (livro.getAutor().getNome().contains(nome)) {
+                livro.imprimir();
+                System.out.println("---");
             }
         }
     }
