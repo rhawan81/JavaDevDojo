7d16570e6d80830b928b8ad640245a4489f657f8
Michel <rhawanjs@gmail.com>
2026-05-14 21:11:34 -0300

    Adiciona a classe Biblioteca com métodos para listar livros e buscar por autor

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Biblioteca.java
@@ -0,0 +1,28 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Hherança.dominio;
+
+public class Biblioteca {
+    private String[] nomes;
+
+    public Biblioteca(String[] nome) {
+        this.nomes = nomes;
+    }
+
+
+
+    public void listarLivros(){
+        System.out.println("Livros disponiveis na biblioteca: ");
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+
+
+    public void BuscarPorAutor(String nome){
+        System.out.println("Buscando livros do autor: " + nome);
+        for (String nomeLivro : nomes) {
+            if(nomeLivro.contains(nome)){
+                System.out.println(nomeLivro);
+            }
+        }
+    }
+}
