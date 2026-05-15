349593bc146455522502e7851e14ffe761a9c76a
Michel <rhawanjs@gmail.com>
2026-05-14 21:07:06 -0300

    Adiciona atributos anoPublicacao e preco à classe Livro e implementa métodos de acesso

--- src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Livro.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Livro.java
@@ -2,4 +2,52 @@ package micheldevDojo.Java_dev_Dojo.javacore.Hherança.dominio;
 
 public class Livro {
     private String titulo;
+    private int anoPublicacao;
+    private double preco;
+    Autor autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void imprimir(){
+        System.out.println(this.titulo);
+        System.out.println(this.anoPublicacao);
+        System.out.println(this.preco);
+        System.out.println(this.autor.getNome());
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public Livro(String titulo, Autor autor, double preco, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+        this.anoPublicacao = anoPublicacao;
+    }
 }
