2d2f18e724c5c23d0359fabf92296655c47ec9d5
Michel <rhawanjs@gmail.com>
2026-05-20 22:52:04 -0300

    Add initialization blocks and logging to Pessoa class

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Isobrescrita/tests/AnimeTest01.java
@@ -0,0 +1,11 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Isobrescrita.tests;
+
+import micheldevDojo.Java_dev_Dojo.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr Stone");
+
+        System.out.println(anime);
+    }
+}
