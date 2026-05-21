eeeb2dbbadece7795635fd20a85a9325b2ab968c
Michel <rhawanjs@gmail.com>
2026-05-20 22:14:03 -0300

    Add initialization blocks and constructor logging to Funcionario class

--- src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Funcionario.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Hherança/dominio/Funcionario.java
@@ -5,8 +5,31 @@ public class Funcionario extends Pessoa {
     private double salario;
 
 
+    static {
+        System.out.println("Dentro do bloco de inicialização estático de Funcionario");
+    }
+
+    {
+        System.out.println("Dentro do bloco de inicializacao de Funcionario 1");
+    }
+
+
+
+    {
+        System.out.println("Dentro do bloco de inicializacao de Funcionario 2");
+    }
+
+
+
+
+
+
+
+
+
     public Funcionario(String nome){
         super(nome);
+        System.out.println("Dentro do construtor de Funcionario");
     }
 
     public Funcionario(String nome, String cpf) {
