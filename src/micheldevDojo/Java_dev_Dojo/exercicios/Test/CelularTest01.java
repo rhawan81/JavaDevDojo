e7f47d73efbb82fde051e7c4b46eee1ab89ddca9
Michel <rhawanjs@gmail.com>
2026-05-21 22:10:00 -0300

    construtor com 3 parametros

--- src/micheldevDojo/Java_dev_Dojo/exercicios/Test/CelularTest01.java
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/CelularTest01.java
@@ -4,7 +4,7 @@ import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.Celular;
 
 public class CelularTest01 {
     public static void main(String[] args) {
-        Celular celular = new Celular("Iphone");
+        Celular celular = new Celular("Iphone","15 Pro" , 2999.90);
         celular.imprime();
 
 
