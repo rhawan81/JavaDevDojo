b9dcd758ed2e3fb5f8e8d56141a3b46f342b5fa2
Michel <rhawanjs@gmail.com>
2026-05-21 22:12:28 -0300

    Add Veiculo class with constructors for marca and ano

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Dominio/Veiculo.java
@@ -0,0 +1,24 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Dominio;
+
+public class Veiculo {
+
+
+    private String marca;
+    private String ano;
+
+
+    public Veiculo(String marca){
+        this.marca = marca;
+    }
+    public Veiculo(String marca, String ano){
+
+        this(marca);
+        this.ano = ano;
+    }
+
+
+
+
+
+
+}
