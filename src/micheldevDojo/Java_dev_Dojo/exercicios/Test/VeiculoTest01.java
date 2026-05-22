2039e3e40b0fbc6bfc006a55bc7b4587efd99869
Michel <rhawanjs@gmail.com>
2026-05-21 22:25:51 -0300

    Add VeiculoTest01 class to test Moto functionality

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/VeiculoTest01.java
@@ -0,0 +1,12 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Test;
+
+import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.Moto;
+
+public class VeiculoTest01 {
+    public static void main(String[] args) {
+        Moto moto = new Moto("Honda", 2020, "CB500X", 29);
+        moto.imprime();
+
+
+    }
+}
