b4c9f9394371b4ce7c20fbcc77ca18a78cb0e283
Michel <rhawanjs@gmail.com>
2026-05-22 19:59:46 -0300

    atributo final que e uma constante

--- src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Carro.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -5,8 +5,9 @@ public class Carro {
     private String nome;
     // CONSTANTE  FINAL NAO PODE TER VALOR ALTERADO E GERALMENTE VEM ACOMPANHADO DO STATIC
     public static final double VELOCIDADE_LIMITE = 250;
+    public final Comprador COMPRADOR = new Comprador();
 
-
+    // Referencia para este objeto nunca poderá ser alterada
     public String getNome() {
         return nome;
     }
