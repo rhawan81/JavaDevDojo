44c7ffdcd581d5d8fe987d8db4e19a3681ba1c23
Michel <rhawanjs@gmail.com>
2026-05-22 20:00:00 -0300

    Add Comprador class instance to carroTest01 and update COMPRADOR name

--- src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/carroTest01.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/carroTest01.java
@@ -1,11 +1,17 @@
 package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.tests;
 
 import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Carro;
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class carroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+        Comprador comprador = new Comprador();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Michel");
+        System.out.println(carro.COMPRADOR);
 
 
     }
