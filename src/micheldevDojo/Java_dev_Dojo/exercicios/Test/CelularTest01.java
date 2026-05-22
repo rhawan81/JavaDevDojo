d3b9a9e7222a743d7564059a75879a52946f2a05
Michel <rhawanjs@gmail.com>
2026-05-21 22:08:33 -0300

    exercicios

---
+++ src/micheldevDojo/Java_dev_Dojo/exercicios/Test/CelularTest01.java
@@ -0,0 +1,27 @@
+package micheldevDojo.Java_dev_Dojo.exercicios.Test;
+
+import micheldevDojo.Java_dev_Dojo.exercicios.Dominio.Celular;
+
+public class CelularTest01 {
+    public static void main(String[] args) {
+        Celular celular = new Celular("Iphone");
+        celular.imprime();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
