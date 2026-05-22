59671f8b6646cb1d0f3f61b563a96be00ee3761b
Michel <rhawanjs@gmail.com>
2026-05-22 20:00:07 -0300

    Add Comprador class with name attribute and toString method

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -0,0 +1,22 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Comprador {
+
+    private String nome;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
