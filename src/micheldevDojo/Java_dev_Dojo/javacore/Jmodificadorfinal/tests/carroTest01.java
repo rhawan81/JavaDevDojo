972de059a3e87c4b2c3172eb67f7e811cf7bf2cc
Michel <rhawanjs@gmail.com>
2026-05-22 19:52:29 -0300

    teste da classe

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/carroTest01.java
@@ -0,0 +1,12 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.tests;
+
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class carroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+
+
+    }
+}
