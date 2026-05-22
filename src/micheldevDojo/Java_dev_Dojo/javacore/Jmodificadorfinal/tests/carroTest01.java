413869969fae83229f8ddd0755642f652fd2768f
Michel <rhawanjs@gmail.com>
2026-05-22 20:09:14 -0300

    Add Ferrari instance to carroTest01 and update its name

--- src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/carroTest01.java
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/carroTest01.java
@@ -2,17 +2,19 @@ package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.tests;
 
 import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Carro;
 import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Comprador;
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class carroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         Comprador comprador = new Comprador();
-
+        Ferrari ferrari = new Ferrari();
         System.out.println(Carro.VELOCIDADE_LIMITE);
         System.out.println(carro.COMPRADOR);
         carro.COMPRADOR.setNome("Michel");
         System.out.println(carro.COMPRADOR);
-
+        ferrari.setNome("Ferrari");
+        ferrari.imprime();
 
     }
 }
