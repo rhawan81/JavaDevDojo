afe5766d72acfe971a37d23fdca995f3fa794557
Michel <rhawanjs@gmail.com>
2026-05-24 20:55:48 -0300

    Add Animal class with methods to emit sound and sleep

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Animal.java
@@ -0,0 +1,23 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Animal {
+    private String nome;
+
+
+
+
+    public void emitirSom(){
+        System.out.println( "Emitindo som...");
+    }
+
+
+    public final void dormir(){
+
+    }
+
+
+
+
+
+
+}
