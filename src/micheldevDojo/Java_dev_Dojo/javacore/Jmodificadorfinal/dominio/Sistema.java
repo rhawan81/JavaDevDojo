54f806d6bdd297accedb6d3249e7cf14ac5e740e
Michel <rhawanjs@gmail.com>
2026-05-24 21:31:12 -0300

    Add Sistema class with notes on inheritance limitations due to final modifier

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Sistema.java
@@ -0,0 +1,7 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Sistema { // aqui seria extends SistemaSeguranca porem segue o texto abaixo explicando o porque
+
+    /// ///// O erro ocorre pois a classe SistemaSeguranca marcada como final nao pode ser herdada
+    // e tambem blqueia o extends
+ }
