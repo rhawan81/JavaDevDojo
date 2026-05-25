5794cf6ce4b679d94203f5a6e431f2bc177f1495
Michel <rhawanjs@gmail.com>
2026-05-24 21:25:49 -0300

    Add CarroTest02 class to test ligar and desligar methods of Carro2

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/CarroTest02.java
@@ -0,0 +1,13 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.tests;
+
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Carro2;
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Veiculo;
+
+public class CarroTest02 {
+    public static void main(String[] args) {
+        Carro2 carro2 = new Carro2();
+        carro2.ligar();
+        carro2.desligar();
+
+    }
+}
