0b61bdf0c8c6ac0fd0beea0eff7aabb9c301cb69
Michel <rhawanjs@gmail.com>
2026-05-24 21:15:46 -0300

    Add CachorroTest01 class to test sound emission and sleeping behavior of Cachorro

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/tests/CachorroTest01.java
@@ -0,0 +1,11 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.tests;
+
+import micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio.Cachorro;
+
+public class CachorroTest01 {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        cachorro.emitirSom();
+        cachorro.dormir();
+    }
+}
