751f27e7270270b925a809bc255a2018f6eebafb
Michel <rhawanjs@gmail.com>
2026-05-24 21:25:31 -0300

    Add Carro2 class extending Veiculo with custom ligar method

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Carro2.java
@@ -0,0 +1,12 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Carro2 extends Veiculo{
+
+    @Override
+    public void ligar(){
+        System.out.println("vrum vrum o carro agora esta ligado!");
+    }
+
+
+
+}
