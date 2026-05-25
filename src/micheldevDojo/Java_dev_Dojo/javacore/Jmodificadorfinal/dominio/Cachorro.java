2df36f904aef7a44fe43b7f24c8d7589fe646408
Michel <rhawanjs@gmail.com>
2026-05-24 21:13:56 -0300

    Add Cachorro class extending Animal with sound emission and sleeping behavior

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Cachorro.java
@@ -0,0 +1,18 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Cachorro extends Animal{
+
+
+    @Override
+public void emitirSom(){
+
+    System.out.println("Au au");
+}
+
+@Override
+public  void dormir(){
+    System.out.println("Cachorro dormindo");
+
+}
+
+}
