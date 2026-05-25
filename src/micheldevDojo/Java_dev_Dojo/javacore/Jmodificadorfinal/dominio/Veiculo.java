4fc34ab8e98951702fdb1ccaa6c19d5010c3ec0e
Michel <rhawanjs@gmail.com>
2026-05-24 21:25:39 -0300

    Add Veiculo class with ligar and final desligar methods

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/Veiculo.java
@@ -0,0 +1,24 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public class Veiculo {
+
+
+    private String nome;
+
+
+    public void ligar(){
+        System.out.println("Veiculo ligado");
+    }
+
+    public final void desligar(){
+        System.out.println("Veiculo desligado");
+    }
+
+
+
+
+
+
+
+
+}
