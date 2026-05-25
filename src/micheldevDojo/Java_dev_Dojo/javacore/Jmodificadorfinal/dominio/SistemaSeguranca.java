bcd26d1e9e64a49fc501aa6ac5db80746c397963
Michel <rhawanjs@gmail.com>
2026-05-24 21:31:17 -0300

    Add SistemaSeguranca class with authentication method

---
+++ src/micheldevDojo/Java_dev_Dojo/javacore/Jmodificadorfinal/dominio/SistemaSeguranca.java
@@ -0,0 +1,10 @@
+package micheldevDojo.Java_dev_Dojo.javacore.Jmodificadorfinal.dominio;
+
+public final class SistemaSeguranca {
+
+
+
+    public void autenticar(){
+        System.out.println("Autenticando...");
+    }
+}
